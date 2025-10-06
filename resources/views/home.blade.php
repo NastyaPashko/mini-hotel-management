04b584a2e43569f2098759c72d9d7c1121744ff1
Nastya231213 <nastya1200526@gmail.com>
2025-10-07 02:03:11 +0300

    feat: create main page and footer layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,79 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1> Welcome to Mini Hotel!</h1>
+    <!-- Weclome Banner -->
+    <main>
+        <section id="welcome-banner" class=" position-relative">
+            <img id="img-banner" src="{{ 'images/hotel-bg.jpg' }}" />
+            <div id="banner-overlay"></div>
+            <div id="banner-content">
+                <h1>Welcome to Mini-Hotel</h1>
+                <p>Your comfort is our priority</p>
+                <div class="d-flex justify-content-center flex-md-row flex-column">
+                    <button class="site-btn">
+                        View Rooms
+                    </button>
+                    <button class="site-btn">
+                        Book Now
+                    </button>
+                </div>
+            </div>
+        </section>
+
+        <!-- Pluses -->
+        <section id="hotel-features" class="container my-5">
+            <h2 class="title"> Why Choose Us</h2>
+            <div class="row mt-5">
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="p-4 rounded shadow-sm bg-white h-100 text-center">
+                        <div class="icon-circle mx-auto mb-3 ">
+                            <svg width="32" height="32" fill="none" stroke="currentColor" stroke-width="1.5"
+                                viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d='M6.133 21C4.955 21 4 20.02 4 18.81v-8.802c0-.665.295-1.295.8-1.71l5.867-4.818a2.09 2.09 0 0 1 2.666 0l5.866 4.818c.506.415.801 1.045.801 1.71v8.802c0 1.21-.955 2.19-2.133 2.19z' />
+                                <path d='M9.5 21v-5.5a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2V21' />
+                            </svg>
+                        </div>
+                        <h5 class="fw-bold">Cozy Atmosphere</h5>
+                        <p class="text-muted">Enjoy a warm, home-like environment where every detail is made for your
+                            comfort.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="p-4 rounded shadow-sm bg-white h-100 text-center">
+                        <div class="icon-circle mx-auto mb-3">
+                            <svg width="32" height="32" fill="none" stroke="currentColor" stroke-width="1.5"
+                                viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d='M12.56 20.82a.96.96 0 0 1-1.12 0C6.611 17.378 1.486 10.298 6.667 5.182A7.6 7.6 0 0 1 12 3c2 0 3.919.785 5.333 2.181 5.181 5.116.056 12.196-4.773 15.64' />
+                                <path d='M12 12a2 2 0 1 0 0-4 2 2 0 0 0 0 4' />
+                            </svg>
+                        </div>
+                        <h5 class="fw-bold">Perfect Location</h5>
+                        <p class="text-muted"> Situated in the city center, our hotel is close to major attractions, cafes,
+                            and transport — everything you need within walking distance.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6  mb-4">
+                    <div class="p-4 rounded shadow-sm bg-white h-100 text-center">
+                        <div class="icon-circle mx-auto mb-3">
+                            <svg width="32" height="32" fill="none" stroke="currentColor" stroke-width="1.5"
+                                viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path d='M12 6.167V4m0 2.167a8 8 0 0 1 8 8V17H4v-2.833a8 8 0 0 1 8-8M3 20h18' />
+                            </svg>
+                        </div>
+                        <h5 class="fw-bold">Exceptional Service</h5>
+                        <p class="text-muted"> Our friendly staff is always ready to help, ensuring your stay is
+                            comfortable, relaxing, and truly memorable.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </main>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mini Hotel Management</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -40,22 +40,26 @@
 
 
                 </ul>
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <div class="d-flex flex-lg-row flex-column ms-lg-auto">
-                        <a class="btn btn-outline-secondary me-1" href="#">
-                            <svg class="btn_icon" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path d='M15 7.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m4.5 13c-.475-9.333-14.525-9.333-15 0' />
+                <ul class="navbar-nav mb-2 mb-lg-0 mx-auto">
+                    <li class="nav-item me-1 mt-2 mt-lg-0">
+                        <a class="site-btn site-btn-green" href="/login">
+                            <svg class="btn_icon" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M15 7.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m4.5 13c-.475-9.333-14.525-9.333-15 0" />
                             </svg>
-                            My account</a>
-                        <a class="btn btn-outline-secondary mt-2 mt-lg-0" href="#"><svg class="btn_icon"
-                                viewBox="0 0 24 24">
+                            Login
+                        </a>
+                    </li>
+                    <li class="nav-item mt-2 mt-lg-0">
+                        <a class="site-btn site-btn-green" href="/register">
+                            <svg class="btn_icon" viewBox="0 0 24 24">
                                 <path
-                                    d='M3.5 15h17M3 9.4c0-2.24 0-3.36.436-4.216a4 4 0 0 1 1.748-1.748C6.04 3 7.16 3 9.4 3h5.2c2.24 0 3.36 0 4.216.436a4 4 0 0 1 1.748 1.748C21 6.04 21 7.16 21 9.4v5.2c0 2.24 0 3.36-.436 4.216a4 4 0 0 1-1.748 1.748C17.96 21 16.84 21 14.6 21H9.4c-2.24 0-3.36 0-4.216-.436a4 4 0 0 1-1.748-1.748C3 17.96 3 16.84 3 14.6z' />
-                            </svg>Admin</a>
-                    </div>
-
+                                    d="M3.5 15h17M3 9.4c0-2.24 0-3.36.436-4.216a4 4 0 0 1 1.748-1.748C6.04 3 7.16 3 9.4 3h5.2c2.24 0 3.36 0 4.216.436a4 4 0 0 1 1.748 1.748C21 6.04 21 7.16 21 9.4v5.2c0 2.24 0 3.36-.436 4.216a4 4 0 0 1-1.748 1.748C17.96 21 16.84 21 14.6 21H9.4c-2.24 0-3.36 0-4.216-.436a4 4 0 0 1-1.748-1.748C3 17.96 3 16.84 3 14.6z" />
+                            </svg>
+                            Register
+                        </a>
+                    </li>
                 </ul>
+
             </div>
         </div>
     </nav>
@@ -64,7 +68,83 @@
         @yield('content')
         </div>
     </main>
+    <footer class="container-fluid bg-white p-5">
+        <div class="row">
+            <div class="col-md-4 mb-3 d-flex flex-column">
+                <div class="d-flex">
+                    <svg class="icon"viewBox="0 0 24 24">
+                        <path
+                            d='M14.7 2c1.68 0 2.52 0 3.162.34a3.06 3.06 0 0 1 1.311 1.359c.327.665.327 1.536.327 3.279v10.044c0 1.743 0 2.614-.327 3.28a3.06 3.06 0 0 1-1.311 1.359C17.22 22 16.38 22 14.7 22H9.3c-1.68 0-2.52 0-3.162-.34a3.06 3.06 0 0 1-1.311-1.359c-.327-.665-.327-1.536-.327-3.279V6.978c0-1.743 0-2.614.327-3.28A3.06 3.06 0 0 1 6.138 2.34C6.78 2 7.62 2 9.3 2z' />
+                        <path d='M9.5 21v-5.5a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2V21M10 6H8m2 4H8m8-4h-2m2 4h-2' />
+                    </svg>
+                    <h2>Mini-Hotel</h2>
+                </div>
+                <p>Your cozy getaway in the heart of the city. Comfort, style, and convenience all in one place.</p>
 
+            </div>
+            <div class="col-6 col-md-2 mb-3">
+                <h5 class="fw-bold">Quick Links</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Rooms</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Services</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Discounts</a>
+                    </li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Dashboard</a>
+                    </li>
+
+                </ul>
+            </div>
+            <div class="col-6 col-md-4 mb-3">
+                <h5 class="fw-bold">Contact</h5>
+                <div> <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="1.5"
+                        viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d='M15.6 14.522c-2.395 2.52-8.504-3.534-6.1-6.064 1.468-1.545-.19-3.31-1.108-4.609-1.723-2.435-5.504.927-5.39 3.066.363 6.746 7.66 14.74 14.726 14.042 2.21-.218 4.75-4.21 2.215-5.669-1.268-.73-3.009-2.17-4.343-.767' />
+                    </svg> +1 (555) 123-4567
+
+                </div>
+                <div>
+                    <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="1.5"
+                        viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d='m2.357 7.714 6.98 4.654c.963.641 1.444.962 1.964 1.087.46.11.939.11 1.398 0 .52-.125 1.001-.446 1.964-1.087l6.98-4.654M7.157 19.5h9.686c1.68 0 2.52 0 3.162-.327a3 3 0 0 0 1.31-1.311c.328-.642.328-1.482.328-3.162V9.3c0-1.68 0-2.52-.327-3.162a3 3 0 0 0-1.311-1.311c-.642-.327-1.482-.327-3.162-.327H7.157c-1.68 0-2.52 0-3.162.327a3 3 0 0 0-1.31 1.311c-.328.642-.328 1.482-.328 3.162v5.4c0 1.68 0 2.52.327 3.162a3 3 0 0 0 1.311 1.311c.642.327 1.482.327 3.162.327' />
+                    </svg> nastya1200526@gmail.com
+                </div>
+                <div>
+                    <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="1.5"
+                        viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d='M12.56 20.82a.96.96 0 0 1-1.12 0C6.611 17.378 1.486 10.298 6.667 5.182A7.6 7.6 0 0 1 12 3c2 0 3.919.785 5.333 2.181 5.181 5.116.056 12.196-4.773 15.64' />
+                        <path d='M10.75 12a1 1 0 0 1-1-1V8.64L12 7l2.25 1.64V11a1 1 0 0 1-1 1z' />
+                    </svg> 123 Luxury Ave, City
+
+                </div>
+            </div>
+            <div class="col-6 col-md-2 mb-3 ">
+                <h5 class="fw-bold">Reception Hours</h5>
+                <p class="mb-1">Monday - Friday: 24/7</p>
+                <p class="mb-0">Saturday - Sunday: 24/7</p>
+            </div>
+
+        </div>
+        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+            <p>© 2025 Company, Inc. All rights reserved.</p>
+            <ul class="list-unstyled d-flex">
+                <li class="ms-3 "><a class="link-body-emphasis" href="#" aria-label="Instagram"><svg
+                            class="bi" width="24" height="24">
+                            <use xlink:href="#instagram"></use>
+                        </svg></a></li>
+                <li class="ms-3"><a class="link-body-emphasis" href="#" aria-label="Facebook"><svg
+                            class="bi" width="24" height="24" aria-hidden="true">
+                            <use xlink:href="#facebook"></use>
+                        </svg></a></li>
+            </ul>
+        </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
